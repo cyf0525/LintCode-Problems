0d895c49febd518f47c9b584fb6b9d0dd9188ec6
YanFengCui <Jens@walbots.com>
2019-07-03 17:15:37 +0800

    q82

---
+++ LintCode82.py
@@ -0,0 +1,12 @@
+class Solution:
+    """
+    @param A: An integer array
+    @return: An integer
+    """
+    def singleNumber(self, A):
+        # write your code here
+        a = 0
+        for i in A:
+            if A.count(i) == 1:
+                return i
+            
