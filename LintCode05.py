943a81c87dd38c46f4c3eeedfeb95a65686219f6
YanFengCui <Jens@walbots.com>
2019-07-03 10:39:09 +0800

    new file

---
+++ LintCode05.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param n: An integer
+    @param nums: An array
+    @return: the Kth largest element
+    """
+    def kthLargestElement(self, n, nums):
+        # write your code here
+        nums.sort(reverse=True)
+        return nums[n-1]
+
